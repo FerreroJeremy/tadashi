b12b98b13aedf86d654944929823791f706c315b
Jérémy Ferrero <ferrero.jerem@gmail.com>
2019-07-23 20:37:54 +0200

    Automatic & periodic training - issue #8
    Respond to issue #8

--- tadashi/train_network.py
+++ tadashi/train_network.py
@@ -1,9 +1,28 @@
 import os
+import datetime
+from utils.lockManager import LockState
+from utils.lockManager import LockManager
+from utils.periodicallyProcessor import PeriodicallyProcessor
 from Keras.Core.kerasManager import KerasManager as Keras
 
 absolute_path = os.path.abspath(os.path.dirname(__file__))
 output_model_path = absolute_path + '/assets/model/model.bin'
 output_label_bin_path = absolute_path + '/assets/model/mlb.pickle'
 
+lock_manager = LockManager(60 * 60 * 24 * 3)
 keras = Keras()
-keras.train(output_model_path, output_label_bin_path)
+
+
+def compute(timestamp):
+    # every 3 days, after 3h of the morning, a new model is trained
+    if datetime.datetime.now().hour > 3 and lock_manager.has_lock('training') == LockState.GOT:
+        keras.train(output_model_path, output_label_bin_path)
+
+
+pp = PeriodicallyProcessor()
+callback = compute
+pp.process(callback)
+
+
+
+
